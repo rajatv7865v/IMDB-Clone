fd6ac9f9500cd79950190441c2859b5637283ee2
Rajat Verma <rajatv785v@gmail.com>
2022-11-25 17:03:11 +0530

    code

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -3,19 +3,21 @@ import "./MovieDetail.css"
 import { useParams } from "react-router-dom"
 
 const Movie = () => {
-    const [currentMovieDetail, setMovie] = useState()
+    const [currentMovieDetail, setMovie] = useState([])
     const { id } = useParams()
-
-    useEffect(() => {
-        getData()
-        window.scrollTo(0,0)
-    }, [getData])
 
     const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
         .then(res => res.json())
         .then(data => setMovie(data))
     }
+
+    useEffect(() => {
+        getData()
+        window.scrollTo(0,0)
+    }, [getData])
+
+   
 
     return (
         <div className="movie">
